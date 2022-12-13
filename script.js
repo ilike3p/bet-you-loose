a0fdfeaf0f125aec7f23c917af3939612ac91436
ilike3p <39431924+ilike3p@users.noreply.github.com>
2022-12-13 00:03:51 -0800

    java updated

---
+++ script.js
@@ -0,0 +1,10 @@
+// Declared variables
+var score = 0;
+var questionIndex = 0;
+
+// Start code 
+// Declared variables
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#blazers");
